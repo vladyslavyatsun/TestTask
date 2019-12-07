c4fc4c994373a473276a71cca751199425e8d046
Vladyslav Yatsun <vladyslavyatsun@gmail.com>
2019-12-07 12:21:19 +0200

    Initial Commit

---
+++ test/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  test
+//
+//  Created by mad on 07.12.2019.
+//  Copyright © 2019 Vladyslav Yatsun. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
